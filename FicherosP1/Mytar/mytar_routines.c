f2b92cf8639f4e3c9c2e461954e9e129c2991f95
Paula Martínez <pamart24@ucm.es>
2021-10-03 16:09:08 +0200

    Práctica 1 terminada Co-Authored-By: Aurora García Jover <aurgar01@ucm.es>

--- FicherosP1/Mytar/mytar_routines.c
+++ FicherosP1/Mytar/mytar_routines.c
@@ -15,16 +15,30 @@ extern char *use;
  * Returns the number of bytes actually copied or -1 if an error occured.
  */
 int
-copynFile(FILE * origin, FILE * destination, int nBytes)
+copynFile(FILE* origin, FILE* destination, int nBytes)
 {
+	//Guardamos el byte desde el que queremos empezar a copiar el fichero
+	//El indicador de posición ya viene asignado antes de llamar al método
+	int byte_inicial = ftell(origin);
+
+	printf("\nIndicador de posicion: %d\n", byte_inicial);
+
 	//Posicionamos el indicador de posición al final del fichero origin
 	if (fseek(origin, 0, SEEK_END) != 0) {
 		printf("\nError en copynFile al hacer fseek del fichero origen\n");
 		fclose(origin);
 		return -1;
 	}
-	//Guardamos los bytes totales de origin
-	int bytes = ftell(origin);
+
+	//Guardamos los bytes que tiene el fichero desde byte_inicial hasta el final
+	int bytes = ftell(origin) - byte_inicial;
+
+	//Volvemos a posicionar el indicador de posición en byte_inicial
+	if (fseek(origin, byte_inicial, SEEK_SET) != 0) {
+		printf("\nError en copynFile al hacer fseek del fichero origen\n");
+		fclose(origin);
+		return -1;
+	}
 
 	//Calculamos cual es el mínimo: nBytes o los bytes del fichero origin
 	if (nBytes < bytes)
@@ -34,9 +48,6 @@ copynFile(FILE * origin, FILE * destination, int nBytes)
 
 	//Reservamos espacio para los bytes del fichero a copiar
 	char* buff = malloc(bytes);
-
-	//Volvemos a colocar el marcador de posición al inicio de origin
-	rewind(origin);
 
 	fread(buff, sizeof(char), bytes, origin);
 	fwrite(buff, sizeof(char), bytes, destination);
@@ -60,8 +71,21 @@ copynFile(FILE * origin, FILE * destination, int nBytes)
 char*
 loadstr(FILE * file)
 {
-	// Complete the function
-	return NULL;
+	char eof;
+	char* buff;
+	int num_bytes = 0;
+	do {
+		eof = getc(file);
+		num_bytes++;	//Número de bytes + \0
+	} while (eof != '\0');
+
+	//Volvemos a colocarnos al comienzo de la palabra
+	fseek(file, -num_bytes, SEEK_CUR);
+
+	buff = malloc(num_bytes);
+	fread(buff, num_bytes, 1, file);
+	
+	return buff;
 }
 
 /** Read tarball header and store it in memory.
@@ -74,12 +98,41 @@ loadstr(FILE * file)
  * the (name,size) pairs read from the tar file. Upon failure, the function returns NULL.
  */
 stHeaderEntry*
-readHeader(FILE * tarFile, int *nFiles)
+readHeader(FILE *tarFile, int *nFiles)
 {
-	// Complete the function
-	//Llama a loadstr
+	stHeaderEntry* header;
+	char* buff;
+	int num_files = 0;
+	
+	//Leemos el número de ficheros N del tarFile y lo volcamos en num_files
+	rewind(tarFile);
+	fread(&num_files, sizeof(int), 1, tarFile);
 
-	return NULL;
+	header = malloc(sizeof(stHeaderEntry) * num_files);
+
+	//Leemos la metainformación del tarFile y la volcamos en el array
+	for (int i = 0; i < num_files; ++i) {
+		//No hace falta reservar tamaño para buff porque ya se hace en el método
+		//de loadstr
+		buff = loadstr(tarFile);
+
+		if (buff == NULL) 
+			return NULL;
+
+		header[i].name = malloc(strlen(buff) + 1);
+		strncpy(header[i].name, buff, strlen(buff) + 1);
+		free(buff);
+			
+		//fread(header[i].name, sizeof(char), num_bytes, tarFile);
+		fread(&header[i].size, sizeof(int), 1, tarFile);
+	}
+	//Necesitamos saltar un byte más para posicionarnos en el primer byte que no es cabecera
+	getc(tarFile);
+
+	//Devolvemos los valores a la función invocadora
+	(*nFiles) = num_files;
+
+	return header;
 }
 
 /** Creates a tarball archive 
@@ -199,6 +252,49 @@ createTar(int nFiles, char *fileNames[], char tarName[])
 int
 extractTar(char tarName[])
 {
-	// Complete the function
-	return EXIT_FAILURE;
+	FILE* mytar;
+	if ((mytar = fopen(tarName, "r")) == NULL) {
+		printf("\nError en extractTar al hacer fopen de mytar\n");
+		return EXIT_FAILURE;
+	}
+
+	int num_files = 0;	
+	stHeaderEntry* header = readHeader(mytar, &num_files);
+
+	if (header == NULL)
+		return EXIT_FAILURE;
+
+	printf("\nAbierto mytar con éxito con num files = %d\n", num_files);
+
+	for (int i = 0; i < num_files; ++i) {
+		//Abrimos el fichero y si no existe, lo crea		
+		FILE* file;
+		if ((file = fopen(header[i].name, "w+")) == NULL) {
+			printf("\nError en extractTar al hacer fopen de uno de los ficheros\n");
+			return EXIT_FAILURE;
+		}
+		
+		//No hay que actualizar la posición del indicador de posición porque ya se
+		//posicionó al final de la cabecera en readHeader	
+		if (copynFile(mytar, file, header[i].size) == -1)
+			return EXIT_FAILURE;
+
+		printf("\nCreado archivo desde mytar\n");
+
+		//Cerramos el fichero creado
+		fclose(file);
+	}
+
+	//Liberamos espacio
+	for (int i = 0; i < num_files; ++i) {
+		free(header[i].name);
+	}
+	free(header);
+
+	//Cerramos el fichero mytar
+	fclose(mytar);
+	
+	printf("\nExtracción desde mytar con éxito\n");
+
+	return EXIT_SUCCESS;
 }
